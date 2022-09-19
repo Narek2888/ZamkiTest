392a7e7229c2694b4fe1754c1ba38227c1eb1059
DAVTYAN Vazgen <vazgen.davtyan@raiffeisen.ru>
2022-09-19 11:00:25 +0400

    add carusel brand

--- src/components/Manufacturers/Manufacturers.jsx
+++ src/components/Manufacturers/Manufacturers.jsx
@@ -1,3 +1,8 @@
+import React, { useEffect, useState } from "react";
+import Marquee from "react-marquee-slider";
+import times from "lodash/times";
+import { nanoid } from "nanoid";
+
 import image from "./image/manufacturer.png";
 import "./Manufacturers.scss";
 
@@ -23,18 +28,29 @@ const Manufacturers = () => {
     },
   ];
 
+  const [key, setKey] = useState(nanoid());
+
+  useEffect(() => {
+    setKey(nanoid());
+  }, []);
+
   return (
     <div className="manufacturers">
       <div className="manufacturers__text">Производители</div>
 
       <div className="manufacturers__logo">
-        {manufacturers.map((item, index) => {
-          return (
-            <div className="manufacturers__logo__item" key={index}>
-              <img src={item.img} alt="manufacturer.png" />
+        <Marquee key={key} velocity={100} initDeg={0}>
+          {times(6, Number).map((id, i) => (
+            <div key={i} className="carusels">
+              <img
+                className="carusel-img"
+                src={manufacturers[id].img}
+                key={`logo-${id}`}
+                alt=""
+              />
             </div>
-          );
-        })}
+          ))}
+        </Marquee>
       </div>
     </div>
   );
