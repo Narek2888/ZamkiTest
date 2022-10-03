357465c90825c4e136abaca5bb2053f7b23b43a9
Arm313 <armbabujyan@gmail.com>
2022-10-04 00:04:11 +0400

    added footer items

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
 import "./Footer.scss";
+import { getDataObj } from "../../utils";
 
 const Footer = () => {
-  const products = [
-    "Замки",
-    "Ручки",
-    "Петли",
-    "Цилиндровые механизмы",
-    "Броненакладки",
-    "Дверная фурнитура",
-    "Доводчики дверей",
-    "Перекодировочные ключи",
-    "Оконная и ПВХ фурнитура",
-    "Мебельная фурнитура",
-    "Кронштейны",
-    "Крючки",
-    "Автопороги",
-  ];
+  const [products, setProducts] = useState({});
+
+  useEffect(() => {
+    getDataObj("https://zamki-strapi.codium.pro/api/footer", setProducts);
+  }, []);
+
+  const {
+    column_1: column__1,
+    column_2: column__2,
+    column_3: column__3,
+    column_4: column__4,
+  } = products;
 
   return (
     <div className="footer">
@@ -26,25 +24,41 @@ const Footer = () => {
           <div className="footer__up__contact__logo">
             <img src={logo} alt="footer_logo" />
           </div>
-
-          <div className="footer__up__contact__item">
-            <div>+7 (495) 153 58 57</div>
-            <div>+7 (910) 464 85 82</div>
-            <div>zakaz@zamkitut.ru</div>
-          </div>
+          {column__1?.length ? (
+            <div className="footer__up__contact__item">
+              {column__1?.map((item, index) => {
+                return <div key={index}>{item}</div>;
+              })}
+            </div>
+          ) : null}
         </div>
 
         <div className="footer__up__products">
           <div className="footer__up__products__text">ПРОДУКЦИЯ</div>
 
           <div className="footer__up__products__items">
-            {products.map((item, index) => {
-              return (
-                <div className="footer__up__products__items__item" key={index}>
-                  {item}
-                </div>
-              );
-            })}
+            {column__2?.length ? (
+              <div className="footer__up__products__items__column2">
+                {column__2?.map((item, index) => {
+                  return <div key={index}>{item}</div>;
+                })}
+              </div>
+            ) : null}
+            {column__3?.length ? (
+              <div className="footer__up__products__items__column3">
+                {column__3?.map((item, index) => {
+                  return <div key={index}>{item}</div>;
+                })}
+              </div>
+            ) : null}
+
+            {column__4?.length ? (
+              <div className="footer__up__products__items__column4">
+                {column__4?.map((item, index) => {
+                  return <div key={index}>{item}</div>;
+                })}
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
